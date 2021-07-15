67024d8a0bc3de50891311c0197fe0adbfab83f6
Gaetan Janssens <janssensgaetan@gmail.com>
2021-07-15 15:35:23 +0200

    user cmd

---
+++ src/DataPersister/UserDataPersister.php
@@ -0,0 +1,109 @@
+<?php
+
+// src/DataPersister
+
+namespace App\DataPersister;
+
+use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
+use App\Entity\Restaurant;
+use App\Entity\User;
+use App\Security\EmailVerifier;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Message;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserDataPersister implements ContextAwareDataPersisterInterface
+{
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $_entityManager;
+
+    /**
+     * @var Request
+     */
+    private $_request;
+
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $_passwordEncoder;
+
+    /**
+     * @var EmailVerifier
+     */
+    private $_emailVerifier;
+
+
+    /**
+     * @var ParameterBagInterface
+     */
+    private $_params;
+
+    /**
+     * UserDataPersister constructor.
+     * @param EntityManagerInterface $entityManager
+     * @param RequestStack $requestStack
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param EmailVerifier $emailVerifier
+     * @param ParameterBagInterface $params
+     */
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        RequestStack $requestStack,
+        UserPasswordEncoderInterface $passwordEncoder,
+        EmailVerifier $emailVerifier,
+        ParameterBagInterface $params
+    ) {
+        $this->_entityManager = $entityManager;
+        $this->_request = $requestStack->getCurrentRequest();
+        $this->_passwordEncoder = $passwordEncoder;
+        $this->_emailVerifier = $emailVerifier;
+        $this->_params = $params;
+    }
+
+    public function supports($data, array $context = []): bool
+    {
+        return $data instanceof User;
+    }
+
+
+    public function persist($data, array $context = [])
+    {
+        if ($data->getPlainPassword()) {
+            $data->setPassword(
+                $this->_passwordEncoder->encodePassword(
+                    $data,
+                    $data->getPlainPassword()
+                )
+            );
+
+            $data->eraseCredentials();
+        }
+
+        $this->_entityManager->persist($data);
+        $this->_entityManager->flush();
+
+        // generate a signed url and email it to the user
+        $this->_emailVerifier->sendEmailConfirmation('app_verify_email', $data,
+            (new TemplatedEmail())
+                ->from(new Address($this->_params->get('app.transactional_mail_sender'), 'Dabba consigne'))
+                ->to($data->getEmail())
+                ->subject('Please Confirm your Email')
+                ->htmlTemplate('registration/confirmation_email.html.twig')
+        );
+        // do anything else you need here, like send an email
+    }
+
+    public function remove($data, array $context = [])
+    {
+        $this->_entityManager->remove($data);
+        $this->_entityManager->flush();
+    }
+}
