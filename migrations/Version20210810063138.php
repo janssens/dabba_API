d950f9392b38cec01016675180e3e0c0d1b6cd28
Gaetan Janssens <janssensgaetan@gmail.com>
2021-08-10 12:06:03 +0200

    restaurant code qr and default zone

---
+++ migrations/Version20210810063138.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210810063138 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE movement ADD quantity INT NOT NULL');
+        $this->addSql('ALTER TABLE stock ADD label VARCHAR(50) DEFAULT NULL, CHANGE type type SMALLINT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE movement DROP quantity');
+        $this->addSql('ALTER TABLE stock DROP label, CHANGE type type INT NOT NULL');
+    }
+}
