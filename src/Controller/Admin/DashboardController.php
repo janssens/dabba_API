a9d1ae1e3e70274b0b80340694dc584593f8dfa6
Gaetan Janssens <janssensgaetan@gmail.com>
2021-10-19 14:54:41 +0200

    admin dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,12 +2,19 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Client;
+use App\Entity\Cms;
 use App\Entity\CodePromo;
 use App\Entity\CodeRestaurant;
+use App\Entity\Color;
+use App\Entity\Container;
+use App\Entity\MealType;
 use App\Entity\Movement;
 use App\Entity\Restaurant;
 use App\Entity\Stock;
 use App\Entity\Tag;
+use App\Entity\User;
+use App\Entity\Zone;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -21,7 +28,7 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        return $this->render('admin/my-dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -33,12 +40,25 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToUrl('Super admin', 'fas fa-mask', $this->generateUrl('super_admin'));
-        yield MenuItem::linkToCrud('The Restaurants', 'fas fa-list', Restaurant::class);
-        yield MenuItem::linkToCrud('Tags restaurant', 'fas fa-tag', Tag::class);
-        yield MenuItem::linkToCrud('Codes restaurant', 'fas fa-key', CodeRestaurant::class);
+
+        yield MenuItem::linkToCrud('Restaurants', 'fas fa-list', Restaurant::class);
+        yield MenuItem::linkToCrud('Types de restaurant', 'fas fa-tag', Tag::class);
+        yield MenuItem::linkToCrud('QR Codes restaurant', 'fas fa-key', CodeRestaurant::class);
         yield MenuItem::linkToCrud('Codes promo', 'fas fa-tag', CodePromo::class);
-        yield MenuItem::linkToCrud('Movements', 'fas fa-arrows-alt-h', Movement::class);
+        yield MenuItem::linkToCrud('Mouvements', 'fas fa-arrows-alt-h', Movement::class);
         yield MenuItem::linkToCrud('Stocks', 'fas fa-cubes', Stock::class);
+
+        if ($this->isGranted('ROLE_SUPER_ADMIN')){
+            yield MenuItem::linkToRoute('Test System API', 'fas fa-key', 'system_pay_check');
+            //yield MenuItem::linkToRoute('My Test', 'fas fa-', 'my_test');
+            yield MenuItem::linkToCrud('API clients', 'fas fa-key', Client::class);
+            yield MenuItem::linkToCrud('Contenus dynamiques', 'fas fa-edit', Cms::class);
+            yield MenuItem::linkToCrud('Types de plat', 'fas fa-tag', MealType::class);
+            yield MenuItem::linkToCrud('Zones', 'fas fa-map-marker-alt', Zone::class);
+            yield MenuItem::linkToCrud('Couleurs', 'fas fa-palette', Color::class);
+            yield MenuItem::linkToCrud('Conteneurs Dadda', 'fas fa-toolbox', Container::class);
+            yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-user', User::class);
+        }
+
     }
 }
