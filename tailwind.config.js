9d50e69a4f803bd2bcb032df191e0be0405e6db3
Gaetan Janssens <janssensgaetan@gmail.com>
2021-11-05 12:37:50 +0100

    Backoffice look

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
     theme: {
         fontFamily: {
             'sans': ['Roboto', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', "Segoe UI", "Helvetica Neue", 'Arial', "Noto Sans", 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+        },
+        extend: {
+            colors: {
+                orange: {
+                    light: '#F99664',
+                    DEFAULT: '#F96C26',
+                    dark: '#f15609',
+                },
+            }
         }
     }
 }
